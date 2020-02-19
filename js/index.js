9a83652378c994d5e1ac10240654d78d8724d034
Abayomi Ogundipe <54248806+babayomi@users.noreply.github.com>
2020-02-20 01:26:58 +0200

    fewd45project-aba

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+$("#grayButton").on("click",grayScheme);
+$("#yellowButton").on("click",yellowScheme);
+
+function grayScheme(){
+    $("body").css("background-color","#414143");
+    $("body").css("color","white");
+    $(".intro-style").css("background-color","#69c5ac");
+}
+
+function yellowScheme(){
+    $("body").css("background-color","#ffd97a");
+    $("body").css("color","#414143");
+    $(".intro-style").css("background-color","white");
+}
